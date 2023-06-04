5a6089ba343e2b20f8699571e47cb26615a8d413
Jakob Holst <jho@miracle.dk>
2023-06-04 23:10:54 +0200

    demo

--- restapi.py
+++ restapi.py
@@ -66,4 +66,5 @@ def update_dns():
        return jsonify({'message': 'Failed to update DNS record.'}), 500
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(host='0.0.0.0', port=5002)
+
